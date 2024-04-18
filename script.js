16b4d8f089f5122b3a6091d65a843ef24824ba09
kushagr14 <klakhanpal14@gmail.com>
2024-04-18 08:58:04 +0530

    made function addScore

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 let score=0, wicket=0;
 
-        function addOne(){
-            score += 1;
-            rootElement.render(<App/>);
-        }
+        // function addOne(){
+        //     score += 1;
+        //     rootElement.render(<App/>);
+        // }
 
-        function addTwo(){
-            score += 2;
-            rootElement.render(<App/>);
-        }
+        // function addTwo(){
+        //     score += 2;
+        //     rootElement.render(<App/>);
+        // }
 
-        function addThree(){
-            score += 3;
-            rootElement.render(<App/>);
-        }
+        // function addThree(){
+        //     score += 3;
+        //     rootElement.render(<App/>);
+        // }
 
-        function addFour(){
-            score += 4;
-            rootElement.render(<App/>);
-        }
+        // function addFour(){
+        //     score += 4;
+        //     rootElement.render(<App/>);
+        // }
 
-        function addFive(){
-            score += 5;
-            rootElement.render(<App/>);
-        }
+        // function addFive(){
+        //     score += 5;
+        //     rootElement.render(<App/>);
+        // }
 
-        function addSix(){
-            score += 6;
+        // function addSix(){
+        //     score += 6;
+        //     rootElement.render(<App/>);
+        // }
+
+        function addScore(num){
+            score += num;
             rootElement.render(<App/>);
         }
 
@@ -40,12 +45,12 @@ let score=0, wicket=0;
                 <h1>Score Keeper App</h1>
                 <h2>Score: {score}/{wicket}</h2>
                 <div>
-                    <button id='1' onClick= {addOne}>1</button>
-                    <button id='2' onClick={addTwo}>2</button>
-                    <button id='3'onClick={addThree}>3</button>
-                    <button id='4'onClick={addFour}>4</button>
-                    <button id='5'onClick={addFive}>5</button>
-                    <button id='6'onClick={addSix}>6</button>
+                    <button id='1' onClick= {() => addScore(1)}>1</button>
+                    <button id='2' onClick={() => addScore(2)}>2</button>
+                    <button id='3'onClick={() => addScore(3)}>3</button>
+                    <button id='4'onClick={() => addScore(4)}>4</button>
+                    <button id='5'onClick={() => addScore(5)}>5</button>
+                    <button id='6'onClick={() => addScore(6)}>6</button>
                     <button id='wicket' onClick={addWicket}>Wicket</button>
                 </div>
                 {
